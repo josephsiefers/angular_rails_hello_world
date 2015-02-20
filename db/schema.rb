ed8d4bbd476e4ff52250238565a33f48004a5d39
Joseph Siefers <joseph.siefers@gmail.com>
2015-02-20 10:19:21 -0500

    Add Employer model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150220151422) do
+ActiveRecord::Schema.define(version: 20150220151847) do
+
+  create_table "employers", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string   "name"
