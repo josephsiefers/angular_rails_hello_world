45f4866ac9eefd1a62a24bd9df7f49c5116e27cc
Joseph Siefers <joseph.siefers@gmail.com>
2015-02-24 15:40:40 -0500

    Add ng-compliant csrf functionality.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  after_filter :set_csrf_cookie_for_ng
+
+  def set_csrf_cookie_for_ng
+  	cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
+protected
+	#http://stackoverflow.com/questions/14734243/rails-csrf-protection-angular-js-protect-from-forgery-makes-me-to-log-out-on
+	#In Rails 4.2 and above
+	def verified_request?
+		super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+	end
+
+	# In Rails 4.1 and below
+	# def verified_request?
+	# 	super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+	# end
 end
