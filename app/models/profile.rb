e734986bd9a1147fbb2be8413a63de960e8a4d49
Joseph Siefers <joseph.siefers@gmail.com>
2015-02-25 15:06:18 -0500

    Add user model.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,5 @@
 class Profile < ActiveRecord::Base
+	belongs_to :user
+	accepts_nested_attributes_for :user
+	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150220174634) do
+ActiveRecord::Schema.define(version: 20150225200051) do
 
   create_table "employers", force: :cascade do |t|
     t.string   "name"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 20150220174634) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
